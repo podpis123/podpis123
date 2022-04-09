2354196d73ee242e7e01652ed33673bc5057a2a0
Miroslav Krupka <krupka6@gmail.com>
2022-04-09 13:17:07 +0200

    app articles add permissions and authorization

--- articles/views.py
+++ articles/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import UpdateView, DeleteView, CreateView
 from django.urls import reverse_lazy
@@ -17,19 +18,33 @@ class ArticleDetailView(DetailView):
     template_name = "article_detail.html"
 
 
-class ArticleUpdateView(UpdateView):
+class ArticleUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Article
     fields = ("title", "body", )
     template_name = "article_edit.html"
+    login_url = 'login'
 
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
 
-class ArticleDeleteView(DeleteView):
+class ArticleDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Article
     template_name = "article_delete.html"
+    login_url = 'login'
     success_url = reverse_lazy("article_list")
 
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
 
-class ArticleCreateView(CreateView):
+
+class ArticleCreateView(LoginRequiredMixin, CreateView):
     model = Article
     template_name = "article_new.html"
-    fields = ("title", "body", "author",)
+    login_url = 'login'
+    fields = ("title", "body",)
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
