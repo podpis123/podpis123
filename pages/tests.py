9834bf2ba4a097aeb699a8513adb18bb57742794
Miroslav Krupka <krupka6@gmail.com>
2022-04-10 14:59:36 +0200

    pages/about tests

--- pages/tests.py
+++ pages/tests.py
@@ -16,3 +16,17 @@ class HomePageTests(SimpleTestCase):
         response = self.client.get(reverse("home"))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "home.html")
+
+class AboutPageTests(SimpleTestCase):
+    def test_about_page_status_code(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_url_by_name(self):
+        response = self.client.get(reverse("about"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse("about"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "about.html")
